968cede67cb72b2958cc91ae7ae1a504e65ad320
denybear <98181634+denybear@users.noreply.github.com>
2022-08-26 20:26:52 +0200

    Add files via upload

--- synthiboocli.sh
+++ synthiboocli.sh
@@ -4,10 +4,11 @@ sleep 10
 export DISPLAY=:0
 export HOME=/home/pi
 export JACK_NO_AUDIO_RESERVATION=1
-jackd --realtime --realtime-priority 70 --port-max 20 --silent -d alsa --device hw:1 --nperiods 3 --rate 48000 --period 64 &
+jackd --realtime --realtime-priority 70 --port-max 30 --silent -d alsa --device hw:1 --nperiods 3 --rate 48000 --period 64 &
 sleep 5
 a2jmidid -ue &
 sleep 5
 /home/pi/boocli/boocli.a /home/pi/boocli/boocli.cfg &
+sleep 5
 export LD_LIBRARY_PATH=/usr/local/lib64:$LD_LIBRARY_PATH
 /home/pi/synthi/synthi.a /home/pi/synthi/synthi.cfg
