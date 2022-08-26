3da804587f6e21ab42d3c171bdcc2854de49a497
denybear <98181634+denybear@users.noreply.github.com>
2022-08-26 19:08:47 +0200

    change in ppq function

---
+++ synthiboocli.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sleep 10
+export DISPLAY=:0
+export HOME=/home/pi
+export JACK_NO_AUDIO_RESERVATION=1
+jackd --realtime --realtime-priority 70 --port-max 20 --silent -d alsa --device hw:1 --nperiods 3 --rate 48000 --period 64 &
+sleep 5
+a2jmidid -ue &
+sleep 5
+/home/pi/boocli/boocli.a /home/pi/boocli/boocli.cfg &
+export LD_LIBRARY_PATH=/usr/local/lib64:$LD_LIBRARY_PATH
+/home/pi/synthi/synthi.a /home/pi/synthi/synthi.cfg
