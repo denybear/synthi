a2defb7f871c93a0ea4927cc8d3c05abb04ce00a
denybear <denybear@free.fr>
2023-01-02 17:31:37 +0100

    removed unused files

--- src/disk.h
+++
@@ -1,8 +0,0 @@
-/** @file disk.h
- *
- * @brief This file defines prototypes of functions inside disk.c
- *
- */
-
-int load ();
-int save ();
--- src/time.h
+++
@@ -1,8 +0,0 @@
-/** @file time.h
- *
- * @brief This file defines prototypes of functions inside time.c
- *
- */
-
-int change_timesign ();
-int time_progress ();
